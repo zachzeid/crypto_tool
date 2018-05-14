bb1f9710f3511c3171e9d83fd8abf05cee24a16d
zach.zeid <zach.zeid@gmail.com>
2018-05-14 14:21:30 -0400

    Big changes, no longer needed to generate ssh keys, I must've been smoking crack, you'd use ssh-keygen to generate any keys you'd need and upload them as necessary with -f.  Also made parameter_name more inclusive instead of trying to separate them out by account_name, team_name, etc.  Reduced amount of arguments.  Also they seemed to have fixed being able to pull ssh keys from parameter store without any futzy encoding issues, so removed encrypting, base64 encoding and encrypting again.  Hopefully this'll be easier to use by various teams.

--- keystore.py
+++ keystore.py
@@ -5,7 +5,6 @@ from base64 import b64encode
 from datetime import datetime
 
 import boto3
-from Crypto.PublicKey import RSA
 
 
 def main():
@@ -14,70 +13,55 @@ def main():
         aws_profile = args.profile[0]
     else:
         aws_profile = 'default'
-
-    if not args.account_name:
-        account_name = get_account_alias(aws_profile)
-        print(account_name)
-    else:
-        account_name = args.account_name[0]
-    team_name = args.team_name
-    generated_key = generate_ssh_keys()
-    store_ssh_keys(args.key_id[0], generated_key,
-                   account_name, team_name, aws_profile)
+    if args.upload:
+        ssh_key_file = file_upload(args.upload[0])
+        print(store_ssh_keys(
+            args.key_id[0],
+            ssh_key_file,
+            args.parameter_name[0],
+            aws_profile
+        ))
 
 
 def arguments():
     parser = argparse.ArgumentParser(description="make magic")
     parser.add_argument('-k', '--key_id', nargs='*', required=True)
-    parser.add_argument('-a', '--account_name', nargs='*')
-    # parser.add_argument('-b', '--key_size', nargs='*')
-    parser.add_argument('-t', '--team_name', nargs='*', required=True)
+    parser.add_argument('-n', '--parameter_name', nargs='*', help="""Name of the
+    SSM Parameter in the format /<account name>/<team>/resource/<ssh_key_name>""")
     parser.add_argument('-p', '--profile', nargs='*')
+    parser.add_argument('-u', '--upload', nargs='*')
     args = parser.parse_args()
     return args
 
 
-def get_account_alias(aws_profile):
-    session = boto3.session.Session(profile_name=aws_profile)
-    iam_client = session.client('iam')
-    account_name = iam_client.list_account_aliases()['AccountAliases'][0]
-    return account_name
+def file_upload(filename):
+    with open(filename, 'r') as f:
+        return f.read()
 
 
-def store_ssh_keys(keyid, keybinary, account_name, team_name, aws_profile):
+def store_ssh_keys(keyid, keybinary, parameter_name, aws_profile):
     session = boto3.session.Session(profile_name=aws_profile)
     ssm_client = session.client('ssm')
     kms_client = session.client('kms')
 
-    encrypt_ssh_key = kms_client.encrypt(
-        KeyId="alias/%s" % keyid, Plaintext=keybinary)
-    encoded_blob = b64encode(encrypt_ssh_key['CiphertextBlob'])
-    ssm_client.put_parameter(
-        Description="Encrypted SSH Key",
-        Type="SecureString",
-        KeyId="alias/%s" % keyid,
-        Name="/{}/{}/resource/sshkey".format(account_name, team_name[0]),
-        Value=encoded_blob.decode('ascii'),
-        Overwrite=True
-    )
+    encrypt_ssh_key = keybinary
+    encoded_blob = encrypt_ssh_key
 
-# Generated SSH private/public keys in the OpenSSH format.
+    try:
+        response = ssm_client.put_parameter(
+            Description="Encrypted SSH Key",
+            Type="SecureString",
+            KeyId="alias/%s" % keyid,
+            Name="/{}".format(parameter_name),
+            Value=encoded_blob,
 
-
-def generate_ssh_keys():
-    dt = datetime.today()
-    print(f"{dt:%Y-%m-%d}")
-    key = RSA.generate(4096)
-
-    with open('private_key_{}'.format(f"{dt:%Y-%m-%d}"), "wb") as private_key_file:
-        private_key_file.write(key.exportKey('OpenSSH'))
-
-    public_key = key.publickey()
-
-    with open('public_key_{}.pub'.format(f"{dt:%Y-%m-%d}"), 'wb') as public_key_file:
-        public_key_file.write(public_key.exportKey('OpenSSH'))
-    # We return this so we can go ahead and encrypt and store into SSM Parameter store.
-    return key.export_key('OpenSSH').decode('ascii')
+            # Value=encoded_blob.decode('ascii'),
+            Overwrite=True
+        )
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            return "Parameter %s sucessfully uploaded." % "/{}".format(parameter_name)
+    except Exception as e:
+        return e
 
 
 if __name__ == '__main__':
