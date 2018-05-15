caf4932f329be5bfa83ae2ce854a6b892432b254
zach.zeid <zach.zeid@gmail.com>
2018-05-15 11:35:50 -0400

    Renamed keystore to something more befitting (though the github name is completely misleading).  Added a get_parameter function, and made key_id not required.

--- parameterstore.py
+++ parameterstore.py
@@ -19,17 +19,42 @@ def main():
             args.parameter_name[0],
             aws_profile
         ))
+    if args.get_parameter:
+        print(get_parameter(aws_profile, args.get_parameter[0]))
 
 
 def arguments():
-    parser = argparse.ArgumentParser(description="make magic")
-    parser.add_argument('-k', '--key_id', nargs='*', required=True)
+    parser = argparse.ArgumentParser(description="""Keystore is a tool to
+    upload sensitive parameters to AWS SSM Parameter Store""")
+    parser.add_argument('-k', '--key_id', nargs='*', help="""
+    The key name for encrypting parameters""")
     parser.add_argument('-n', '--parameter_name', nargs='*', help="""Name of the
-    SSM Parameter in the format /<account name>/<team>/resource/<ssh_key_name>""")
-    parser.add_argument('-p', '--profile', nargs='*')
-    parser.add_argument('-u', '--upload', nargs='*')
+    SSM Parameter in the format /<account name>/<team>/<type>/<resource_name>""")
+    parser.add_argument('-p', '--profile', nargs='*', help="""AWS profile Name
+    and credentials to be used.""")
+
+    parser.add_argument('-u', '--upload', nargs='*', help="""Name of file to be
+    uploaded as a parameter.""")
+    parser.add_argument('-g', '--get_parameter', nargs='*', help=""" Name of the SSM
+    parameter to be fetched.""")
     args = parser.parse_args()
     return args
+
+
+def get_parameter(aws_profile, parameter_name):
+    session = boto3.session.Session(profile_name=aws_profile)
+    ssm_client = session.client('ssm')
+    kms_client = session.client('kms')
+    try:
+        response = ssm_client.get_parameters(
+            Names=[
+                parameter_name
+            ],
+            WithDecryption=True
+        )
+        return response['Parameters'][0]['Value']
+    except Exception as e:
+        return e
 
 
 def file_upload(filename):
